5e46b99b55476c8d73cd603368c306d82334b9ef
Karan Lodaya <148992626+KaranLodaya@users.noreply.github.com>
2025-01-11 16:05:24 +0530

    Update settings.py

--- Ecommerce/Ecommerce/settings.py
+++ Ecommerce/Ecommerce/settings.py
@@ -69,8 +69,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER ='karanlodaya363@gmail.com'
-EMAIL_HOST_PASSWORD ='xavs qbfg baoc tngc'
+EMAIL_HOST_USER =''
+EMAIL_HOST_PASSWORD =''
 
 
 TEMPLATES = [
